3f18328d4c38364e4058d28b1f7b2f2c874c89dd
ElizaKorzhovska <ipz252_kev@student.ztu.edu.ua>
2026-01-11 16:24:15 +0200

    Створено похідний клас Motorcycle

---
+++ ConsoleApp1/Motorcycle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Motorcycle : Vehicle
+    {
+        public int EngineVolume;
+        public bool HasWindshield;
+        public Motorcycle(string brand, string model, int year, double price, int engineVolume, bool hasWindshield)
+            : base(brand, model, year, price)
+        {
+            EngineVolume = engineVolume;
+            HasWindshield = hasWindshield;
+        }
+        public int GetEngineVolume() { return EngineVolume; }
+        public bool GetHasWindshield() { return HasWindshield; }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Мотоцикл: {Brand} {Model} {Year}");
+            Console.WriteLine($"Ціна: {Price} грн");
+            Console.WriteLine($"Об'єм двигуна: {EngineVolume} см³");
+            Console.WriteLine($"Вітрове скло: {(HasWindshield ? "Так" : "Ні")}");
+            Console.WriteLine($"Максимальна швидкість: {GetMaxSpeed()} км/год");
+            Console.WriteLine($"Транспортний податок: {CalculateTax()} грн");
+        }
+        public override double CalculateTax()
+        {
+            return Price * 0.008 + EngineVolume * 0.5;
+        }
+        public override int GetMaxSpeed()
+        {
+            return 200;
+        }
+    }
+}
