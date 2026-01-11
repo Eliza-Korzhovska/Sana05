37a6e3ad9d01cb7308a4116bbd3e6c3ed37253b2
ElizaKorzhovska <ipz252_kev@student.ztu.edu.ua>
2026-01-11 15:26:09 +0200

    Створено базовий клас Vehicle

---
+++ ConsoleApp1/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Vehicle
+    {
+        public string Brand;
+        public string Model;
+        public int Year;
+        public double Price;
+        public Vehicle(string brand, string model, int year, double price)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Price = price;
+        }  
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"{Brand} {Model} {Year}");
+            Console.WriteLine($"Ціна: {Price} грн");
+        }
+        public virtual double CalculateTax()
+        {
+            return Price * 0.01;
+        }
+        public virtual int GetMaxSpeed()
+        {
+            return 100;
+        }
+    }
+}
